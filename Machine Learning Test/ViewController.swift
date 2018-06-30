daf580ecfc5e7fb44c53cc3d73e68b524e310f9c
karmadeli <chadkarmadeli@gmail.com>
2018-06-30 17:40:43 -0400

    tweaks

--- Machine Learning Test/ViewController.swift
+++ Machine Learning Test/ViewController.swift
@@ -12,47 +12,37 @@ import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var confidence: UILabel!
+    @IBOutlet weak var AIReturn: UILabel!
+    @IBOutlet weak var imageVIew: UIImageView!
+    @IBOutlet weak var instructions: UILabel!
+
     
     let imagePicker = UIImagePickerController()
-   
-    @IBOutlet weak var confidence: UILabel!
-    
-    @IBOutlet weak var AIReturn: UILabel!
-    
-    
+
     //tells the delegate, ie our viewcontroller class, that user has picked an image.
     //The info parameter contains the image that the user picked.
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        
         if let imageUserPicked = info[UIImagePickerControllerOriginalImage] as? UIImage{
             imageVIew.image = imageUserPicked
-            
             guard let ciimage = CIImage(image: imageUserPicked) else{fatalError("ERROR could convert to cimage")}
             
             detect(image: ciimage)
         }
         
-        
         imagePicker.dismiss(animated: true, completion: nil)
-
-        
     }
     
-    
-    @IBOutlet weak var instructions: UILabel!
     func detect(image: CIImage){
-       
         do{
             let model = try VNCoreMLModel(for: Inceptionv3().model)
             let request = VNCoreMLRequest(model: model) { (request, error) in
 //VNClassificationObservation: hold classifaction observations and possible matches
                 guard let results = request.results as? [VNClassificationObservation] else{fatalError("model failled to process image")}
-                print("🤡 \(results)")
-                
+ 
                 if let firstResult = results.first{
                     print("\(String(describing: results.first?.confidence))")
-                    print("🍟\(firstResult.identifier)🍟")
-                    
+ 
                     let con = results.first!.confidence*100
                     let bestResults = firstResult.identifier
                  
@@ -73,7 +63,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             let handler = VNImageRequestHandler(ciImage: image)
             do{
                 try handler.perform([request])
-                print("📟Request performed")}
+ 
+            }
                 
             catch{print(error.localizedDescription)}
         }
@@ -81,27 +72,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         catch let error{
             print (error)
         }
-        
-        
-        
-        
     }
     
     @IBAction func cameraTapped(_ sender: Any) {
         present(imagePicker, animated: true, completion: nil)
     }
     
-    @IBOutlet weak var imageVIew: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
         confidence.text = ""
         AIReturn.text = ""
-        
        
         //source type could also be .photolibrary
         imagePicker.sourceType = .camera
